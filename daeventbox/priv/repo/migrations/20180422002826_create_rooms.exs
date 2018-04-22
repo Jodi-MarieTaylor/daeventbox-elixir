aebaaae499b7bb49ebf446cf800abee0baa388f7
sirfitz <romario@Romarios-MacBook-Pro.local>
2018-04-22 17:24:16 -0500

    added chat capability

--- daeventbox/priv/repo/migrations/20180422002826_create_rooms.exs
+++ daeventbox/priv/repo/migrations/20180422002826_create_rooms.exs
@@ -5,6 +5,7 @@ defmodule Daeventbox.Repo.Migrations.CreateRooms do
     create table(:rooms) do
       add :name, :string
       add :owner_id, :integer
+      add :recipient_id, :integer
 
       timestamps()
     end
